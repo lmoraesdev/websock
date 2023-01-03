fa5907a3b2e5f1f06f531be32ce6271804171fea
lmoraesdev <lbc_moraes@outlook.com>
2023-01-03 19:34:52 -0300

    feat: banco funcionado e atualizado

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,4 +1,4 @@
-import { documentosColecao } from "./dbConnect.js";
+import { atualizaDocumento, encontrarDocumento } from "./documentosDb.js"
 import io from "./servidor.js";
 
 io.on("connection", (socket) => {
@@ -10,25 +10,18 @@ io.on("connection", (socket) => {
 
 socket.on("selecionar_documento", async (nomeDocumento, devolverTexto) => {
 	socket.join(nomeDocumento);
-
 	const documento = await encontrarDocumento(nomeDocumento);
 	if (documento) {
 		devolverTexto(documento.texto);
 	}
 });
 
-	socket.on("texto_editor", ({ texto, nomeDocumento }) => {
-		const documento = encontrarDocumento(nomeDocumento);
-		if (documento) {
-			documento.texto = texto;
+	socket.on("texto_editor",async ({ texto, nomeDocumento }) => {
+		const atualizacao = await atualizaDocumento(nomeDocumento, texto);
+		if (atualizacao.modifiedCount) {
 			socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
 		}
 	});
 });
 
-function encontrarDocumento(nome) {
-	const documento = documentosColecao.findOne({
-		nome,
-	});
-	return documento;
-}
+
